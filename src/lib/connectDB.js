42400e5c5de0313fd2a63986547f074c9874990f
anupaisuru <anupaisuru2002@gmail.com>
2025-07-02 18:16:12 +0530

    create mongo connection

---
+++ src/lib/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+export const connectDB = async () => {
+    try {
+        if(connection.isConnected){
+            console.log("use existing connection")
+            return;
+        }
+        const db = await mongoose.connect(process.env.Mongo_URI);
+        connection.isConnected = db.connections[0].readyState;
+    } catch (error) {
+        console.log(error)
+    }
+}
